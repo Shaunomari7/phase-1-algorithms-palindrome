8d8592ad31d3aea4f170d567393a6ae14fcd389a
Shaun Omari <shaunomari72@gmail.com>
2023-10-05 12:45:43 +0300

    algorithm made

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split("")
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString = reverseLetters.join("")
+
+  if(word==reverseString) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
@@ -8,7 +17,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-*/
+  firstly the array of letters is split into a single letters
+then the received word is converted into a reversed version of the original
+after, the reversed word is joined to an array or a word
+then the reversed word and the received word are checked to see if they are similar and true/false values are returned accordingly
+*/ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
